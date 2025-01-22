4754eb19d35075ee05b1249dac4d8fa4e5e84355
klaviwka <klaviwka@mail.ru>
2025-01-22 21:27:45 +0300

    Delete LessonDjango17_1 directory

--- LessonDjango17_1/UrbanDjango/UrbanDjango/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from UrbanDjango.task2.views import index, Index2
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('class/', Index2.as_view(), name='index2'),
-]
